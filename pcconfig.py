6a6362da3b62d3591fee393d9d84391bb2b28b5b
molmoty <molmoty@gmail.com>
2023-04-12 02:24:26 +0900

    config

--- pcconfig.py
+++ pcconfig.py
@@ -1,7 +1,27 @@
 import pynecone as pc
+import os
+from dotenv import load_dotenv
 
+
+def get_mysql_db_url():
+    host = os.environ.get("DB_HOST")
+    port = os.environ.get("DB_PORT")
+    username = os.environ.get("DB_USERNAME")
+    password = os.environ.get("DB_PASSWORD")
+    db_name = os.environ.get("DB_NAME")
+
+    assert all(
+        filter(lambda i: i is not None, (username, password, db_name))
+    ), "DB 정보를 정확히 입력해주세요."
+
+    return f"mysql+pymysql://{username}:{password}@{host}:{port}/{db_name}"
+
+
+load_dotenv()
 config = pc.Config(
     app_name="app",
-    db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
+    deploy_url=os.environ.get("DEPLOY_URL") or None,
+    db_url=get_mysql_db_url(),
+    env=os.environ.get("APP_ENVIRONMENT") or pc.Env.DEV,
+    api_url=os.environ.get("BACKEND_URL") or pc.constants.API_URL,
 )
